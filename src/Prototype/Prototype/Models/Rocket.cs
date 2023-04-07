50a30e111d0963b8c9dcfa8644c507db47bfd053
xasnet <xasnet@gmail.com>
2023-04-07 22:10:46 +0300

    [PT-1].Создал прототипы космического транспорта

---
+++ src/Prototype/Prototype/Models/Rocket.cs
@@ -0,0 +1,32 @@
+﻿using Prototype.Interfaces;
+
+namespace Prototype.Models
+{
+    public class Rocket : RocketStage, IMyCloneable<Rocket>
+    {
+        private const int RocketFuelTank = 1500;
+        protected bool HasAstronaut;
+
+        public Rocket(string name, int speed) : base(name, speed)
+        {
+            HasFuel = true;
+            HasAstronaut = true;
+            Fuel = RocketFuelTank;
+        }
+
+        private Rocket(Rocket rocket)
+            : this(rocket.Name, rocket.Speed)
+        {
+        }
+
+        public override Rocket MyClone()
+        {
+            return new Rocket(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Rocket Name: {Name}, Speed: {Speed}, Fuel: {Fuel}, HasFuel: {HasFuel}, HasAstronaut: {HasAstronaut}";
+        }
+    }
+}
---
+++ src/Prototype/Prototype/Program.cs
@@ -0,0 +1,41 @@
+﻿using Prototype.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var spaceTransport = new SpaceTransport("Космический транспорт", 0);
+        var rocketStage = new RocketStage("Ступень ракеты", 7900);
+        var rocket = new Rocket("Ракета Восток", 7900);
+
+        Console.WriteLine(spaceTransport);
+        Console.WriteLine(rocketStage);
+        Console.WriteLine(rocket);
+        Console.WriteLine();
+
+        var spaceTransportClone_1 = (SpaceTransport)spaceTransport.Clone();
+        spaceTransportClone_1.Name += "_1";
+        var rocketStageClone_1 = (RocketStage)rocketStage.Clone();
+        rocketStageClone_1.Name += "_1";
+        var rocketClone_1 = (Rocket)rocket.Clone();
+        rocketClone_1.Name += "_1";
+
+        Console.WriteLine(spaceTransportClone_1);
+        Console.WriteLine(rocketStageClone_1);
+        Console.WriteLine(rocketClone_1);
+        Console.WriteLine();
+
+        var spaceTransportClone_2 = (SpaceTransport)spaceTransportClone_1.MyClone();
+        spaceTransportClone_2.Name += "_2";
+        var rocketStageClone_2 = (RocketStage)rocketStageClone_1.MyClone();
+        rocketStageClone_2.Name += "_2";
+        var rocketClone_2 = (Rocket)rocketClone_1.MyClone();
+        rocketClone_2.Name += "_2";
+
+        Console.WriteLine(spaceTransportClone_2);
+        Console.WriteLine(rocketStageClone_2);
+        Console.WriteLine(rocketClone_2);
+
+        Console.ReadKey();
+    }
+}
